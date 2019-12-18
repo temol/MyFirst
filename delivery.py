6209f171adf8b7db018e05099cc5488b12d45816
WangRongtao <temol@sina.com>
2019-12-18 16:10:20 +0800

    conf

--- delivery.py
+++ delivery.py
@@ -3,16 +3,28 @@
 """
 import redis
 import time
+from models import MeterModel
 
 
 if __name__ == '__main__':
     pool = redis.ConnectionPool(host='redis', max_connections=5)
     client = redis.Redis(connection_pool=pool)
 
-    print('pop')
-    while True:
-        time.sleep(0.1)
+    res_list = [
+        '1110129000001',
+        '1110129000002',
+        '1110129000003',
+        '1110129000004',
+        '1110129000005',
+        '1110129000006'
+    ]
+    for res in res_list:
+        m = MeterModel.instance(res, client, "key")
+        print(m.ResKey, m.InputTime, m.ResType)
 
-        x = client.blpop("message", 120)
-        if x:
-            print('pop from [message]:  %s' % (x[1].decode(),))
+    for res in res_list:
+        m = MeterModel.instance(res, client, "set")
+        print(m.meterPrice, m.serverIP, m.serverPort)
+
+    while True:
+        time.sleep(30)
--- store.py
+++ store.py
@@ -7,6 +7,7 @@ import datetime
 import pymssql
 import decimal
 import threading
+import time
 
 
 class DateEncoder(json.JSONEncoder):
@@ -24,42 +25,81 @@ class DateEncoder(json.JSONEncoder):
 class DatabaseOperation:
     @staticmethod
     def get(redis_pool, db_conn):
-        pass
+        client = redis.Redis(connection_pool=redis_pool)
+        cursor = db_conn.cursor(as_dict=True)
+        while True:
+            time.sleep(0.1)
+            content = client.blpop("db_get", 60)
+            if not content:
+                continue
+            content = content.decode('utf-8')
+            res_id, tag = content.split('_')
+            if tag == "key":
+                cursor.execute("select * from tb_meter where ResID=%s", res_id)
+                data = cursor.fetchone()
+                if data:
+                    res_info = json.dumps(data, cls=DateEncoder)
+                    client.set(content, res_info)
+            if tag == "set":
+                cursor.execute("select * from tb_meter_set where ResId=%s", res_id)
+                data = cursor.fetchone()
+                if data:
+                    res_info = json.dumps(data, cls=DateEncoder)
+                    client.set(content, res_info)
 
     @staticmethod
     def update(redis_pool, db_conn):
-        pass
+        client = redis.Redis(connection_pool=redis_pool)
+        cursor = db_conn.cursor(as_dict=True)
+        while True:
+            time.sleep(0.1)
+            content = client.blpop("db_update", 60)
+            if not content:
+                continue
+            content = content.decode('utf-8')
+            res_id, tag = content.split('_')
+            if tag == "key":
+                v = client.get(content)
+                if not v:
+                    continue
+                res_info = json.loads(v)
+                sql, params = DatabaseOperation.build_sql("tb_meter", res_info, res_id)
+                cursor.execute(sql, params)
+                db_conn.commit()
+            if tag == "set":
+                v = client.get(content)
+                if not v:
+                    continue
+                res_info = json.loads(v)
+                sql, params = DatabaseOperation.build_sql("tb_meter_set", res_info, res_id)
+                cursor.execute(sql, params)
+                db_conn.commit()
+
+    @staticmethod
+    def build_sql(table_name, info, res_id):
+        sql = "update " + table_name + " set "
+        key_list = info.keys()
+        val_list = list()
+        for k in key_list:
+            sql += k + "=%s,"
+            val_list.append(info[k])
+        sql = sql[0:-1] + " where ResId=%s"
+        val_list.append(res_id)
+        return sql, tuple(val_list)
 
 
 if __name__ == '__main__':
-    pool = redis.ConnectionPool(host='10.27.0.8', max_connections=5)
-    client = redis.Redis(connection_pool=pool)
-
+    pool = redis.ConnectionPool(host='redis', max_connections=5)
+    # client = redis.Redis(connection_pool=pool)
+    conn_get = pymssql.connect("10.27.0.2", "sa", "P@ssw0rd", "NBMeter")
+    conn_update = pymssql.connect("10.27.0.2", "sa", "P@ssw0rd", "NBMeter")
     # 建立线程处理
-    get_thread = threading.Thread(target=DatabaseOperation.get, args=(pool,))
-    update_thread = threading.Thread(target=DatabaseOperation.update, args=(pool,))
+    get_thread = threading.Thread(target=DatabaseOperation.get, args=(pool, conn_get))
+    update_thread = threading.Thread(target=DatabaseOperation.update, args=(pool, conn_update))
 
     get_thread.start()
     update_thread.start()
 
-    a = dict()
-    with pymssql.connect('10.27.0.2', 'sa', 'P@ssw0rd', 'NBMeter') as conn:
-        cursor = conn.cursor(as_dict=True)
-        cursor.execute("select * from tb_meter_set")
-        data = cursor.fetchall()
-        if data:
-            for row in data:
-                res_id = row.pop("ResId") + "_info"
-                res_info = json.dumps(row, cls=DateEncoder)
-                # if not client.exists(res_id):
-                client.set(res_id, res_info)
-                print("get tb_meter_set", res_id, res_info)
+    while True:
+        time.sleep(10)
 
-        cursor.execute("select * from tb_meter")
-        data = cursor.fetchall()
-        if data:
-            for row in data:
-                res_id = row.pop("ResID") + "_key"
-                res_key = json.dumps(row, cls=DateEncoder)
-                client.set(res_id, res_key)
-                print("get tb_meter:", res_id, res_key)
